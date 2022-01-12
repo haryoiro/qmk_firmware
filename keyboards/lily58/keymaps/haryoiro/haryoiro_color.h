fd86ac1ad54c35e33d5338a2d3ecf75ea71b5e9b
haryoiro <mizusecocolte@gmail.com>
2022-01-12 14:06:36 +0900

    Add custom color for myself

---
+++ keyboards/lily58/keymaps/haryoiro/haryoiro_color.h
@@ -0,0 +1,35 @@
+/* Copyright 2022 Haryoiro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TEAL                0x00, 0x80, 0x81
+#define JUNGLE_GREEN        0x26, 0x9A, 0x8D
+#define OCEAN_GREEN         0x4D, 0xB3, 0x8D
+#define MEDIUM_AQUAMARINE   0x73, 0xCD, 0xA6
+#define TEAL_DEER           0x99, 0xE6, 0xB2
+
+#define HARYOIRO_HSV_TEAL                180, 100, 50
+#define HARYOIRO_HSV_JUNGLE_GREEN        173, 75,  60
+#define HARYOIRO_HSV_OCEAN_GREEN         165, 56,  70
+#define HARYOIRO_HSV_MEDIUM_AQUAMARINE   154, 43,  80
+#define HARYOIRO_HSV_TEAL_DEER           139, 33,  178
+
+#define GRAD_TEAL_0   HARYOIRO_HSV_TEAL
+#define GRAD_TEAL_1   HARYOIRO_HSV_JUNGLE_GREEN
+#define GRAD_TEAL_2   HARYOIRO_HSV_OCEAN_GREEN
+#define GRAD_TEAL_3   HARYOIRO_HSV_MEDIUM_AQUAMARINE
+#define GRAD_TEAL_4   HARYOIRO_HSV_TEAL_DEER
